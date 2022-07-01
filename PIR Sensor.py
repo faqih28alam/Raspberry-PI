cc4b52a5ccb163766b2670650de1074d302a721c
Faqih <80795963+faqih28alam@users.noreply.github.com>
2022-07-01 16:47:36 +0700

    Add files via upload

---
+++ PIR Sensor.py
@@ -0,0 +1,17 @@
+#https://maker.pro/raspberry-pi/tutorial/how-to-interface-a-pir-motion-sensor-with-raspberry-pi-gpio
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.IN)         #Read output from PIR motion sensor
+#GPIO.setup(11, GPIO.OUT)         #LED output pin
+while True:
+    i=GPIO.input(16)
+    if i==0:                 #When output from motion sensor is LOW
+        print ("No intruders",i)
+        #GPIO.output(3, 0)  #Turn OFF LED
+        time.sleep(0.1)
+    elif i==1:               #When output from motion sensor is HIGH
+        print ("Intruder detected",i)
+        #GPIO.output(3, 1)  #Turn ON LED
+        time.sleep(0.1)
