70bc130eb3c9c9dc0f14c15d518e43a34455d42d
Faqih <80795963+faqih28alam@users.noreply.github.com>
2022-07-23 14:13:35 +0700

    Update RaspberryPi Send Image from files to Firebase.py

--- Send Images To Firebase/RaspberryPi Send Image from files to Firebase.py
+++ Send Images To Firebase/RaspberryPi Send Image from files to Firebase.py
@@ -7,14 +7,14 @@ import os
 import pyrebase
 
 firebaseConfig = {
-    'apiKey': "AIzaSyAMniwDKsFBnUk9E1d3Ac3BAUsCIN35lFc",
-    'authDomain': "faqsendimages.firebaseapp.com",
-    'databaseURL': "https://faqsendimages-default-rtdb.firebaseio.com",
-    'projectId': "faqsendimages",
-    'storageBucket': "faqsendimages.appspot.com",
-    'messagingSenderId': "756140925503",
-    'appId': "1:756140925503:web:2b676767423c9f912c4e8a",
-    'measurementId': "G-BJYR2GCEZ3"
+    'apiKey': ".....",
+    'authDomain': ".....",
+    'databaseURL': ".....",
+    'projectId': ".....",
+    'storageBucket': ".....",
+    'messagingSenderId': ".....",
+    'appId': ".....",
+    'measurementId': "....."
 
 }
 
