ba8c6d55e78dd4c91f793b0de6e92d7001429374
Faqih <80795963+faqih28alam@users.noreply.github.com>
2022-08-24 21:45:57 +0700

    Update PiCamera.py
    the camera preview only works when a monitor is connected to your Raspberry Pi.

--- PiCamera.py
+++ PiCamera.py
@@ -1,3 +1,6 @@
+#the camera preview only works when a monitor is connected to your Raspberry Pi.
+#reference : https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/4
+
 from picamera import PiCamera
 from time import sleep
 
